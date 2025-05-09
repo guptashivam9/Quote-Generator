f7e0c25c53a69b21b9c759f699cbb938bd93460d
guptashivam9 <shivam98042@gmail.com>
2025-05-09 18:44:55 +0545

    At first I create all Html and CSS requried to make this app and then in JS file Array of quotes are created, currentIndex(i) is set as 0 at first,CurentCategory is set as all at first along with fontsize of 18px

---
+++ main.js
@@ -0,0 +1,15 @@
+const quotes = [
+    { text: "The good thing about science is that it's true whether or not you believe in it.", category: "science" },
+    { text: "Science is simply the word we use to describe a method of organizing our curiosity.", category: "science" },
+    { text: "Programs must be written for people to read, and only incidentally for machines to execute.", category: "computer" },
+    { text: "Computer science is no more about computers than astronomy is about telescopes.", category: "computer" }
+  ];
+  
+  let currentIndex = 0;
+  let currentCategory = 'all';
+  let fontSize = 18;
+  
+
+
+
+  
